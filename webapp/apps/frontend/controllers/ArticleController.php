eb3abc71eaf37d8f05698df67aba4bbcbaf016bd
VietNH <vietpiano@gmail.com>
2016-08-10 16:49:20 +0700

    a

--- webapp/apps/frontend/controllers/ArticleController.php
+++ webapp/apps/frontend/controllers/ArticleController.php
@@ -19,28 +19,58 @@ class ArticleController extends ControllerBase
 
         $query = "atid={$data->id}";
         $listcat = AtCat::find(array('conditions' => $query));
+        $listcat = $listcat->toArray();
         $detailType = "detail";
         $relatedCount = 5;
         if($data->types=="audio") {
             $detailType = "audio";
             $relatedCount = 4;
+            if(count($listcat)>0){
+                $query = 'Webapp\Frontend\Models\Article.types = :type: and Webapp\Frontend\Models\Article.id != :atid: and \Webapp\Frontend\Models\AtCat.catid = :catid: 
+                    and  Webapp\Frontend\Models\Article.status = 1';
+                $bind = array('catid' => $listcat[0]['catid'] ,'type'=>"audio",'atid'=>$data->id);
+            }
+            else{
+                $query = 'Webapp\Frontend\Models\Article.types = :type: and Webapp\Frontend\Models\Article.id != :atid: and  Webapp\Frontend\Models\Article.status = 1';
+                $bind = array('type'=>"audio",'atid'=>$data->id);
+            }
+            $relatedpost = AtCat::query()
+                ->leftJoin('Webapp\Frontend\Models\Article', 'Webapp\Frontend\Models\AtCat.atid = Webapp\Frontend\Models\Article.id')
+                ->where($query,$bind)
+                ->orderBy('Webapp\Frontend\Models\Article.datecreate DESC')
+                ->limit($relatedCount,0)
+                ->execute();
         }
         else if($data->types=="video"){
             $detailType = "video";
             $relatedCount = 4;
+            if(count($listcat)>0){
+                $query = 'Webapp\Frontend\Models\Article.types = :type: and Webapp\Frontend\Models\Article.id != :atid: and \Webapp\Frontend\Models\AtCat.catid = :catid: 
+                    and  Webapp\Frontend\Models\Article.status = 1';
+                $bind = array('catid' => $listcat[0]['catid'] ,'type'=>"video",'atid'=>$data->id);
+            }
+            else{
+                $query = 'Webapp\Frontend\Models\Article.types = :type: and Webapp\Frontend\Models\Article.id != :atid: and  Webapp\Frontend\Models\Article.status = 1';
+                $bind = array('type'=>"audio",'atid'=>$data->id);
+            }
+            $relatedpost = AtCat::query()
+                ->leftJoin('Webapp\Frontend\Models\Article', 'Webapp\Frontend\Models\AtCat.atid = Webapp\Frontend\Models\Article.id')
+                ->where($query,$bind)
+                ->orderBy('Webapp\Frontend\Models\Article.datecreate DESC')
+                ->limit($relatedCount,0)
+                ->execute();
         }
-        /*if(count($listcat)<=0) $detailType = "detail";
         else{
+            $query = 'atid != '.$data->id.' AND catid = '.$listcat-[0]['catid'];
+            $relatedpost = AtCat::find(
+                array(
+                    'conditions' => $query,
+                    'order' => 'atid DESC',
+                    'limit' => $relatedCount
+                )
+            );
+        }
 
-        }*/
-        $query = $listcat->toArray() ? 'atid != '.$data->id.' AND catid = '.$listcat->toArray()[0]['catid'] : 0;
-        $relatedpost = AtCat::find(
-            array(
-                'conditions' => $query,
-                'order' => 'atid DESC',
-                'limit' => $relatedCount
-            )
-        );
         $viewdata = new \stdClass();
 
         $viewdata->coverphoto = $data->coverphoto ? $data->coverphoto : $this->config->media->host.'uploads/default-image/article-cover.jpg';
